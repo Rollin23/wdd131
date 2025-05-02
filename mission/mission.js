38b6ae963b14ff11e28427b830b11b97b622450c
rollin23 <rollin23@byui.edu>
2025-05-02 12:52:37 -0600

    classes and logo

--- mission/mission.js
+++ mission/mission.js
@@ -1,28 +1,20 @@
 
 //This script is to change the dolors of the page when the theme selector has fark selected
 
-const themeSelector = document.querySelector('#theme-selector')//(Hint: document.querySelector)
+const themeSelector = document.querySelector('#theme-selector');
+const logoImg = document.querySelector('.logoImg');
+
 function changeTheme() {
     const selectedTheme = themeSelector.value
-// check to see what the current value of our select is.
-// The current value is conveniently found in themeSelector.value!
+
     if (selectedTheme === 'dark') {
-        ReportBody.classList.add('dark');
-        img.src = byui-logo_white.png
-    }
-    else {
-        ReportBody.classList.remove('dark')
-        img.src = logo.webp
+        document.body.classList.add('dark');
+        logoImg.src = 'byui-logo_white.png';
+    } else {
+        document.body.classList.remove('dark');
+        logoImg.src = 'logo.webp';
     }
 
-// if the value is dark then:
-// add the dark class to the body
-// change the source of the logo img to point to the white logo.
-// otherwise
-// remove the dark class
-// make sure the logo src is the blue logo.
 }
 
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
 themeSelector.addEventListener('change', changeTheme);
